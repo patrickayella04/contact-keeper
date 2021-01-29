a3a5d846830c60eee32927b4e03a1f5914a2f592
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-01-29 22:38:04 +0000

    Refactoring code in data base

--- config/db.js
+++ config/db.js
@@ -2,18 +2,34 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-    mongoose.connect(db, {
-        // Parameters added to avoid warnings in the version of mongoose. How you might not need these after a certain version.
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    })
-        .then(() => console.log('MongoDB Connected'))
-        .catch(err => {
-            console.error(err);
-            process.exit(1); // Will Exit with failure
+const connectDB = async () => {
+    // Clearner code with async await and try catch. 
+    try {
+        await mongoose.connect(db, {
+            // Parameters added to avoid warnings in the version of mongoose. How you might not need these after a certain version.
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
         });
+
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.error(err);
+        process.exit(1); // Will Exit with failure
+    }
+
+    // Less cleaner code -
+    // mongoose.connect(db, {
+    //     // Parameters added to avoid warnings in the version of mongoose. How you might not need these after a certain version.
+    //     useNewUrlParser: true,
+    //     useCreateIndex: true,
+    //     useFindAndModify: false
+    // })
+    //     .then(() => console.log('MongoDB Connected'))
+    //     .catch(err => {
+    //         console.error(err);
+    //         process.exit(1); // Will Exit with failure
+    //     });
 };
 
 module.exports = connectDB;
