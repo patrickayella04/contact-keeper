01c758188e4d7b8ece53997c01763dfd91093d50
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-02-02 03:53:40 +0000

    code

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
