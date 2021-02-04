b76031591b0b78d46f954cf5ca1904ad8436a82c
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-02-04 12:36:15 +0000

    code

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,7 +4,8 @@ const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
-const Contact = require('../models/Contact')
+const Contact = require('../models/Contact');
+const { restart } = require('nodemon');
 
 
 // A signiture for each route so we know whats going on - 
@@ -30,7 +31,7 @@ router.get('/',auth, async (req, res) => { // we add an endpoint/url
 //@access  Private - you have to be logged in to get them
 router.post('/',
     [
-    auth,
+    auth, 
         [
             check('name', 'Name is required')
                 .not()
@@ -55,7 +56,7 @@ router.post('/',
             });
 
             // Save new instance of contact in data base
-            const contact = await newContact.saved();
+            const contact = await newContact.save();
 
             res.json(contact);
         } catch (err) {
@@ -69,15 +70,70 @@ router.post('/',
 //@route   PUT api/contacts/:id 
 //@desc    Update contact
 //@access  Private - you have to be logged in to get them
-router.put('/:id',(req, res) => { // we add an endpoint/url
-    res.send('Update contact');
+router.put('/:id', auth, async (req, res) => { // we add an endpoint/url
+    const { name, email, phone, type } = req.body;
+
+
+    // Build contact object -
+    // Check if these fields are submitted and then add to the contact fields
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        // We access parameters like '/:id' using req.params.id which will find the contact by id. 
+        let contact = await Contact.findById(req.params.id); 
+
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+        // We need to make sure the user owns the contact. We dont want someone else to update anyone elses contact. (req.user.id is a string, so we convert contact.user to a string also with stString())
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        //Options Object: 1. ($set) Update contact: 2. (new) If its a new contact true, then just create it.
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFields },
+            { new: true });
+        
+        res.json(contact) // we send new/updated contact
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+
 });
 
 //@route   PUT api/contacts/:id 
 //@desc    Delete contact
 //@access  Private - you have to be logged in to get them
-router.delete('/:id',(req, res) => { // we add an endpoint/url
-    res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => { // we add an endpoint/url
+    
+
+    try {
+        // We access parameters like '/:id' using req.params.id which will find the contact by id. 
+        let contact = await Contact.findById(req.params.id); 
+
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+        // We need to make sure the user owns the contact. We dont want someone else to update anyone elses contact. (req.user.id is a string, so we convert contact.user to a string also with stString())
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        
+        await Contact.findByIdAndRemove(req.params.id);
+            
+        
+            res.json({ msg: 'Contact removed' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
 });
 
 
