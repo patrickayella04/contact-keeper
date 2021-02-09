402c6337a87c8a6119cea4349b6abf9fef0f1b3e
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-02-09 08:36:07 +0000

    code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // Connect Database
 const connectDB = require('./config/db');
-//const path = require('path');
+const path = require('path');
 
 // Initialise express into a variable called app
 
@@ -29,12 +29,12 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production - all we are doing is checking the evironment to see if its in production. Then loading a static react build folder. Then have a route to the home page, it will load the index.html page inside the client build folder.  
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-// }
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 // Then this app object will have app listen method which will take in a PORT to listen on. We create a variable for the port so we can have two places to find our port. One for production and one for development. Port 5000 will be for development, where as with env.PORT it will look for an environment variable called  PORT first which will be used inn production...but we say OR (||) and say we can use any port we want i.e. 5000.
 const PORT = process.env.PORT || 5000;
